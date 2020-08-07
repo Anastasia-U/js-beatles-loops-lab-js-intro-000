2eeab2a1282d11c528c25ddda596fc71e3ecde70
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-08-07 22:53:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 function theBeatlesPlay(musicians, instruments) {
   var music = [];
   for (var i = 0; i < musicians.length; i++) {
-    music.push(`${musicians[i] plays ${instruments[i]}}`)
+    music.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-    return music
+    return music;
 }
 
 function johnLennonFacts(facts) {
