ff9a85c97747088d2abb8e8abe683a1475cc6345
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-08-07 22:51:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments) {
-  var kvas = [];
-  for (var i = 0; i < musicians.length; i++){
-
-    kvas.push(`${musicians[i]} plays ${instruments[i]}`);
-
+  var music = [];
+  for (var i = 0; i < musicians.length; i++) {
+    music.push(`${musicians[i] plays ${instruments}}`)
   }
-  return kvas;
+
 }
 
 function johnLennonFacts(facts) {
-  var factsL = [];
-  var n = 0;
-  while (n < facts.length) {
-    var kvas = facts[n] + "!!!";
-    factsL.push(kvas);
-    n++;
-  }
-  return factsL;
 }
 
 function iLoveTheBeatles(number) {
-  var beet = [];
 
-  do {
-    number++;
-    beet.push(`I love the Beatles!`);
-  } while(number < 15);
-
-
-  return beet;
 }
